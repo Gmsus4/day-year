30fed0cb330730f3e48524a2479ddb9e2aac91ba
Fernando <jesusfgm45@gmail.com>
2024-06-15 23:55:29 -0600

    Cambios en el tamaño de la pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,22 @@ import { months } from 'moment/moment';
 function App() {
   return (
     <>
-    <div className='flex flex-col items-center justify-between h-[90vh]'>
-      <div>
-        <span>{moment().format('l')}</span>
-      </div>
-      <div className='flex flex-col gap-6'>
-        <h1 className='text-9xl'>
-          {moment().dayOfYear()}
-        </h1>
-        <span className='text-6xl'>{moment().week()}</span>
-        <span className='text-3xl'>{moment().format('YYYY')}</span>
-      </div>
-      <div>
-        <span>{moment().format('LT')}</span>
-        
+    <div className='flex flex-col items-center justify-center h-[85vh]'>
+      <div className='flex flex-col items-center justify-between h-[600px]'>
+        <div>
+          <span>{moment().format('l')}</span>
+        </div>
+        <div className='flex flex-col gap-6'>
+          <h1 className='text-9xl'>
+            {moment().dayOfYear()}
+          </h1>
+          <span className='text-6xl'>{moment().week()}</span>
+          <span className='text-3xl'>{moment().format('YYYY')}</span>
+        </div>
+        <div>
+          <span>{moment().format('LT')}</span>
+          
+        </div>
       </div>
     </div>
     </>
